10a5aff2936161853e31d0cda37d88bcb62415a3
Vasyl Teliman <vasniktel@gmail.com>
2020-10-21 01:10:55 +0300

    Minor fixes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import telegram as tg
 import telegram.ext
 import logging
 import datanews
+import re
 
 PERSISTENCE_FILE_NAME = 'datanewsbot'
 
@@ -18,23 +19,27 @@ logger.setLevel(logging.INFO)
 
 
 def get_usage() -> str:
-    return (
-        '/help - show help\n'
-        f'/{SEARCH_COMMAND} <query> - retrieve news articles containing <query>. Example: "/{SEARCH_COMMAND} covid"\n'
-        f'/{PUBLISHER_COMMAND} - retrieve newest articles by publisher. Example: "/{PUBLISHER_COMMAND} techcrunch.com"\n'
-    )
+    return f'''This bot allows you to query news articles from Datanews API.
+
+Available commands:
+/help, /start - show this help message.
+/{SEARCH_COMMAND} <query> - retrieve news articles containing <query>.
+   Example: "/{SEARCH_COMMAND} covid"
+/{PUBLISHER_COMMAND} <domain> - retrieve newest articles from <publisher>.
+   Example: "/{PUBLISHER_COMMAND} techcrunch.com"
+'''
 
 
-def start(update: tg.update.Update, context: tg.ext.CallbackContext) -> None:
+def help_command(update: tg.update.Update, context: tg.ext.CallbackContext) -> None:
     update.message.reply_markdown(get_usage())
 
 
 def _fetch_data(update: tg.update.Update, context: tg.ext.CallbackContext, fetcher: Callable[[str], Any]) -> None:
     if not context.args:
-        start(update, context)
+        help_command(update, context)
         return
 
-    query = '"' + " ".join(context.args) + '"'
+    query = '"' + ' '.join(context.args) + '"'
     result = fetcher(query)
 
     if result['status'] == 401:
@@ -54,23 +59,30 @@ def _fetch_data(update: tg.update.Update, context: tg.ext.CallbackContext, fetch
 def search_command(update: tg.update.Update, context: tg.ext.CallbackContext) -> None:
     def fetcher(query: str) -> Any:
         return datanews.headlines(query, size=10, sortBy='date', page=0, language='en')
-    return _fetch_data(update, context, fetcher)
+    _fetch_data(update, context, fetcher)
 
 
 def publisher_command(update: tg.update.Update, context: tg.ext.CallbackContext) -> None:
     def fetcher(query: str) -> Any:
         return datanews.headlines(source=query, size=10, sortBy='date', page=0, language='en')
-    return _fetch_data(update, context, fetcher)
+    _fetch_data(update, context, fetcher)
 
 
 def main(token: str) -> None:
     persistence = tg.ext.PicklePersistence(filename=PERSISTENCE_FILE_NAME)
     updater: tg.ext.Updater = tg.ext.Updater(token=token, persistence=persistence)
 
-    updater.dispatcher.add_handler(tg.ext.CommandHandler('start', start))
-    updater.dispatcher.add_handler(tg.ext.CommandHandler('help', start))
+    updater.dispatcher.add_handler(tg.ext.CommandHandler('start', help_command))
+    updater.dispatcher.add_handler(tg.ext.CommandHandler('help', help_command))
     updater.dispatcher.add_handler(tg.ext.CommandHandler(SEARCH_COMMAND, search_command))
     updater.dispatcher.add_handler(tg.ext.CommandHandler(PUBLISHER_COMMAND, publisher_command))
+
+    updater.dispatcher.add_handler(
+        tg.ext.MessageHandler(
+            tg.ext.Filters.text & tg.ext.Filters.regex(pattern=re.compile('help', re.IGNORECASE)),
+            help_command
+        )
+    )
 
     updater.start_polling()
     updater.idle()
@@ -81,6 +93,7 @@ if __name__ == '__main__':
 
     if not sys.argv or len(sys.argv) != 3:
         print("Usage: python main.py <api key> <telegram token>")
+        exit(1)
 
     datanews.api_key = sys.argv[1]
     main(sys.argv[2])
